3ab2197961e9f230e7066ae816ebe58385c467bc
Najim Uddin <engr.najim3@gmail.com>
2023-07-05 22:53:52 +0600

    3. Signup to aws for using SES & S3, Send email using AWS SES

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,5 @@
+import * as config from "../config.js";
+
 export const welcome = (req, res) => {
   res.json({
     data: "hello from nodejs api from routes...",
@@ -10,12 +12,37 @@ export const preRegister = async (req, res) => {
   try {
     console.log(req.body);
 
-    const emailSent = false;
-    if (emailSent) {
-      return res.json({ ok: true });
-    } else {
-      return res.json({ ok: false });
-    }
+    config.AWSSES.sendEmail(
+      {
+        Source: config.EMAIL_FROM,
+        Destination: {
+          ToAddresses: ["najim.eng.3@gmail.com"],
+        },
+        Message: {
+          Body: {
+            Html: {
+              Charset: "UTF-8",
+              Data: `
+             <h1>Welcome to Realist App</h1>
+            `,
+            },
+          },
+          Subject: {
+            Charset: "UTF-8",
+            Data: "Welcome to Realist",
+          },
+        },
+      },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          return res.json({ ok: false });
+        } else {
+          console.log(data);
+          return res.json({ ok: true });
+        }
+      }
+    );
   } catch (error) {
     console.log(error);
     return res.json({ error: "Something went wrong. Try again." });
